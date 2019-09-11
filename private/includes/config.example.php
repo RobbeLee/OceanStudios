a7b5fc5d837e6c2cd53d0f7b935b57a05dd04eba
MikeS25933 <25933@ma-web.nl>
2019-09-11 14:38:44 +0200

    Update config.php

--- private/includes/config.example.php
+++ private/includes/config.example.php
@@ -7,7 +7,8 @@ $config = [
     'DB_PASSWORD' => 'root',
     'ROOT' => dirname(dirname(__DIR__)),
     'PRIVATE' => dirname(__DIR__),
-    'WEBROOT' => dirname(__DIR__)
+    'WEBROOT' => dirname(__DIR__),
+    'BASE_RULE' => dirname(__DIR__) . '/public/'
 ];
 
 return $config;
