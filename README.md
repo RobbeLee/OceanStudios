824ea7dcc299195e933333718f643b3f1764caeb
Bjørn Emmaneel <27728@ma-web.nl>
2019-09-12 13:49:29 +0200

    Link van AdobeXD toegevoegd aan de README.md voor Pascal
    En in portofolio heb ik de templates toegevoegd

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # OceanStudios
 Een github repository voor onze duurzame fashion design website.
+
+Link [adobeXD]
+
+[adobeXD]: https://xd.adobe.com/spec/55f503d5-aff1-427b-5b98-137680269514-23c4/grid
--- private/views/portofolio.php
+++ private/views/portofolio.php
@@ -5,4 +5,16 @@
  * Date: 2019-09-11
  * Time: 15:10
  */
+?>
+<?php $this->layout('standard_layout') ?>
 
+<?php $this->start('css') ?>
+<link rel="stylesheet" href="<?php echo url('css/portofolio.css') ?>">
+<?php $this->stop('css') ?>
+
+<?php $this->start('page_title') ?>
+portofolio
+<?php $this->stop('page_title') ?>
+
+<!-- Hier komt de html code -->
+<!-- Dit komt in .main te staan -->
