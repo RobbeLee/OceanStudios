2c37e3b06547d10189652a2279c8acc074d2b286
MikeS25933 <25933@ma-web.nl>
2019-09-13 19:13:47 +0200

    Fixed path issues (UPDATE YOUR CONFIG.PHP)
    UPDATE YOUR CONFIG.PHP

--- private/includes/config.example.php
+++ private/includes/config.example.php
@@ -5,10 +5,10 @@ $config = [
     'DB_NAME' => 'oceanstudios',
     'DB_USER' => 'root',
     'DB_PASSWORD' => 'root',
+	'BASE_URL' => '',
     'ROOT' => dirname(dirname(__DIR__)),
     'PRIVATE' => dirname(__DIR__),
-    'WEBROOT' => dirname(__DIR__),
-    'BASE_RULE' => dirname(__DIR__) . '/public/'
+    'WEBROOT' => dirname(dirname(__DIR__)) . '/public'
 ];
 
 return $config;
--- private/includes/functions.php
+++ private/includes/functions.php
@@ -12,14 +12,31 @@ function dbConnect($config) {
     }
 }
 
-function url( $path ) {
-	global $CONFIG;
-	return $CONFIG['BASE_URL'] . $path;
-}
-
 function get_template_engine() {
 	global $CONFIG;
 	$templates_path  = $CONFIG['PRIVATE'] . '/views';
 	$template_engine = new League\Plates\Engine( $templates_path );
 	return $template_engine;
+}
+
+function base_url() {
+    global $CONFIG;
+    // base directory
+    $public_dir = preg_replace("!${_SERVER['SCRIPT_NAME']}$!", '', $_SERVER['SCRIPT_FILENAME']);
+    // server protocol
+    $protocol = empty($_SERVER['HTTPS']) ? 'http' : 'https';
+    // domain name
+    $domain = $_SERVER['SERVER_NAME'];
+    // base url
+    $base_url = preg_replace("!^${public_dir}!", '', $CONFIG['WEBROOT']);
+    // server port
+    $port = $_SERVER['SERVER_PORT'];
+    $disp_port = (($protocol === 'http' && $port === 80) || ($protocol === 'https' && $port === 443)) ? '' : ":$port";
+    // put em all together to get the complete base URL
+    $url = "${protocol}://${domain}/";
+    return $url;
+}
+
+function url($path) {
+    return base_url() . $path;
 }
