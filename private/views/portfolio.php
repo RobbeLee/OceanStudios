a90abf99ab030d9132cfe0d56e723d508d0fb6f1
Hensikt <bjornemmaneel@gmail.com>
2019-09-16 10:50:55 +0200

    Update to portfolio.php

--- private/views/portfolio.php
+++ private/views/portfolio.php
@@ -9,7 +9,7 @@
 <?php $this->layout('standard_layout') ?>
 
 <?php $this->start('css') ?>
-<link rel="stylesheet" href="<?php echo url('css/portfolio.css') ?>">
+<link rel="stylesheet" href="<?php echo url('OceanStudios/public/css/portfolio.css') ?>">
 <?php $this->stop('css') ?>
 
 <?php $this->start('page_title') ?>
@@ -18,3 +18,22 @@ Portfolio | Ocean Studios
 
 <!-- Hier komt de html code -->
 <!-- Dit komt in .main te staan -->
+
+<div class="main__header">
+    <div class="main__blauw">
+        <div class="main__blauw--img">
+
+        </div>
+        <div class="main__blauw--content">
+
+        </div>
+    </div>
+    <div class="main__wit">
+        <div class="main__wit--img">
+
+        </div>
+        <div class="main__wit--content">
+
+        </div>
+    </div>
+</div>
