7ae71f74e45dcd23cfe05f2c35d7a0a4a6e1c8fa
Hensikt <bjornemmaneel@gmail.com>
2019-09-16 10:44:02 +0200

    Spellings correcties + mogelijk idee voor portfolio.php
    Kleine update

---
+++ private/views/portfolio.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hensikt
+ * Date: 2019-09-11
+ * Time: 15:10
+ */
+?>
+<?php $this->layout('standard_layout') ?>
+
+<?php $this->start('css') ?>
+<link rel="stylesheet" href="<?php echo url('OceanStudios/public/css/portfolio.css') ?>">
+<?php $this->stop('css') ?>
+
+<?php $this->start('page_title') ?>
+<?php $this->stop('page_title') ?>
+
+<!-- Hier komt de html code -->
+<!-- Dit komt in .main te staan -->
+
+    <div class="main__header">
+        <img src="" alt="">
+    </div>
+    <div class="main__project--blauw">
+        <div class="main__project--img">
+            <img src="" alt="">
+        </div>
+        <div class="main__project--content">
+
+        </div>
+    </div>
+
+    <div class="main__project--wit">
+        <div class="main__project--img">
+            <img src="" alt="">
+        </div>
+        <div class="main__project--content">
+        
+        </div>
+    </div>
