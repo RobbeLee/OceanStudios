a09cea5d2827c16f9a03ecd0f4d70ebe0574f94f
Robbe <26785@ma-web.nl>
2019-09-16 11:43:54 +0200

    Updated shop files
    Started working on the frontend of the webshop.

--- private/views/shop/homepage.php
+++ private/views/shop/homepage.php
@@ -1,12 +1,179 @@
 <?php $this->layout('standard_layout') ?>
 
 <?php $this->start('css') ?>
-<link rel="stylesheet" href="<?php echo url('css/index.css') ?>">
+<link rel="stylesheet" href="<?php echo url('OceanStudios/public/css/shop.css') ?>">
 <?php $this->stop('css') ?>
 
 <?php $this->start('page_title') ?>
-Ocean Studios
+Shop | Ocean Studios
 <?php $this->stop('page_title') ?>
 
-<!-- Hier komt de html code -->
-<!-- Dit komt in .main te staan -->
+<div class="items">
+    <div class="item">
+        <img src="https://via.placeholder.com/200x400" alt="weg, gone, loesoe">
+        <div class="item__body">
+            <div class="item__info">
+                <h3 class="item__title"></h3>
+                <span class="item__price">$35,00</span>
+                <select class="item__size" name="" id="">
+
+                </select>
+            </div>
+            <div class="item__btn-container">
+                <button class="item__btn"></button>
+            </div>
+        </div>
+    </div>
+    <div class="item">
+        <img src="https://via.placeholder.com/200x400" alt="weg, gone, loesoe">
+        <div class="item__body">
+            <div class="item__info">
+                <h3 class="item__title"></h3>
+                <span class="item__price">$35,00</span>
+                <select class="item__size" name="" id="">
+
+                </select>
+            </div>
+            <div class="item__btn-container">
+                <button class="item__btn"></button>
+            </div>
+        </div>
+    </div>
+    <div class="item">
+        <img src="https://via.placeholder.com/200x400" alt="weg, gone, loesoe">
+        <div class="item__body">
+            <div class="item__info">
+                <h3 class="item__title"></h3>
+                <span class="item__price">$35,00</span>
+                <select class="item__size" name="" id="">
+
+                </select>
+            </div>
+            <div class="item__btn-container">
+                <button class="item__btn"></button>
+            </div>
+        </div>
+    </div>
+    <div class="item">
+        <img src="https://via.placeholder.com/200x400" alt="weg, gone, loesoe">
+        <div class="item__body">
+            <div class="item__info">
+                <h3 class="item__title"></h3>
+                <span class="item__price">$35,00</span>
+                <select class="item__size" name="" id="">
+
+                </select>
+            </div>
+            <div class="item__btn-container">
+                <button class="item__btn"></button>
+            </div>
+        </div>
+    </div>
+    <div class="item">
+        <img src="https://via.placeholder.com/200x400" alt="weg, gone, loesoe">
+        <div class="item__body">
+            <div class="item__info">
+                <h3 class="item__title"></h3>
+                <span class="item__price">$35,00</span>
+                <select class="item__size" name="" id="">
+
+                </select>
+            </div>
+            <div class="item__btn-container">
+                <button class="item__btn"></button>
+            </div>
+        </div>
+    </div>
+    <div class="item">
+        <img src="https://via.placeholder.com/200x400" alt="weg, gone, loesoe">
+        <div class="item__body">
+            <div class="item__info">
+                <h3 class="item__title"></h3>
+                <span class="item__price">$35,00</span>
+                <select class="item__size" name="" id="">
+
+                </select>
+            </div>
+            <div class="item__btn-container">
+                <button class="item__btn"></button>
+            </div>
+        </div>
+    </div>
+    <div class="item">
+        <img src="https://via.placeholder.com/200x400" alt="weg, gone, loesoe">
+        <div class="item__body">
+            <div class="item__info">
+                <h3 class="item__title"></h3>
+                <span class="item__price">$35,00</span>
+                <select class="item__size" name="" id="">
+
+                </select>
+            </div>
+            <div class="item__btn-container">
+                <button class="item__btn"></button>
+            </div>
+        </div>
+    </div>
+    <div class="item">
+        <img src="https://via.placeholder.com/200x400" alt="weg, gone, loesoe">
+        <div class="item__body">
+            <div class="item__info">
+                <h3 class="item__title"></h3>
+                <span class="item__price">$35,00</span>
+                <select class="item__size" name="" id="">
+
+                </select>
+            </div>
+            <div class="item__btn-container">
+                <button class="item__btn"></button>
+            </div>
+        </div>
+    </div>
+    <div class="item">
+        <img src="https://via.placeholder.com/200x400" alt="weg, gone, loesoe">
+        <div class="item__body">
+            <div class="item__info">
+                <h3 class="item__title"></h3>
+                <span class="item__price">$35,00</span>
+                <select class="item__size" name="" id="">
+
+                </select>
+            </div>
+            <div class="item__btn-container">
+                <button class="item__btn"></button>
+            </div>
+        </div>
+    </div>
+    <div class="item">
+        <img src="https://via.placeholder.com/200x400" alt="weg, gone, loesoe">
+        <div class="item__body">
+            <div class="item__info">
+                <h3 class="item__title"></h3>
+                <span class="item__price">$35,00</span>
+                <select class="item__size" name="" id="">
+
+                </select>
+            </div>
+            <div class="item__btn-container">
+                <button class="item__btn"></button>
+            </div>
+        </div>
+    </div>
+    <div class="item">
+        <img src="https://via.placeholder.com/200x400" alt="weg, gone, loesoe">
+        <div class="item__body">
+            <div class="item__info">
+                <h3 class="item__title"></h3>
+                <span class="item__price">$35,00</span>
+                <select class="item__size" name="" id="">
+
+                </select>
+            </div>
+            <div class="item__btn-container">
+                <button class="item__btn"></button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!--https://via.placeholder.com/200x400 -->
