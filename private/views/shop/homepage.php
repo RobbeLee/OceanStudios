ce8b3f38caf37414d5cb2b4abed198a3f330c4ba
Robbe <26785@ma-web.nl>
2019-09-18 17:06:52 +0200

    Updated the shop
    Updated homepage.php and shop.css. Website looks now as it should on mobile.

--- private/views/shop/homepage.php
+++ private/views/shop/homepage.php
@@ -16,80 +16,92 @@ Shop | Ocean Studios
 <div class="items shirts">
     <h2 class="title"> <img src="./img/swoosh.png" alt="loes" class="title__img"> T-shirts</h2>
     <div class="item">
-        <img src="https://via.placeholder.com/200x400" alt="weg, gone, loesoe">
+        <img src="https://via.placeholder.com/200x400?text=wide%20boy" alt="weg, gone, loesoe">
         <div class="item__body">
             <div class="item__info">
                 <h3 class="item__title">Ocean T-shirt</h3>
                 <span class="item__price">€35,00</span>
-                <select class="item__amount" name="" id="">
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                </select>
-                <select class="item__size" name="" id="">
-                    <option value="xs">Extra small</option>
-                    <option value="s">Small</option>
-                    <option value="m">Medium</option>
-                    <option value="l">Large</option>
-                    <option value="xl">Extra large</option>
-                </select>
-            </div>
-            <div class="item__btn-container">
+                <div class="select-container">
+                    <select class="item__amount" name="" id="">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                    </select>
+                    <svg viewBox="0 0 20 10"><path d="M0,2 10,10 20,2 18,0 10,6 2,0"/></svg>
+                </div>
+                <div class="select-container">
+                    <select class="item__size" name="" id="">
+                        <option value="xs">XS</option>
+                        <option value="s">S</option>
+                        <option value="m">M</option>
+                        <option value="l">L</option>
+                        <option value="xl">XL</option>
+                    </select>
+                    <svg viewBox="0 0 20 10"><path d="M0,2 10,10 20,2 18,0 10,6 2,0"/></svg>
+                </div>
                 <button class="item__btn"><img src="./img/swoosh.png" alt="loes" class="item__btn--img"> Bestel nu</button>
             </div>
         </div>
     </div>
     <div class="item">
-        <img src="https://via.placeholder.com/200x400" alt="weg, gone, loesoe">
+        <img src="https://via.placeholder.com/200x400?text=wide%20boy" alt="weg, gone, loesoe">
         <div class="item__body">
             <div class="item__info">
                 <h3 class="item__title">Ocean T-shirt</h3>
                 <span class="item__price">€35,00</span>
-                <select class="item__amount" name="" id="">
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                </select>
-                <select class="item__size" name="" id="">
-                    <option value="xs">Extra small</option>
-                    <option value="s">Small</option>
-                    <option value="m">Medium</option>
-                    <option value="l">Large</option>
-                    <option value="xl">Extra large</option>
-                </select>
-            </div>
-            <div class="item__btn-container">
-                <button class="item__btn" ><img src="./img/swoosh.png" alt="loes" class="item__btn--img">Bestel nu</button>
+                <div class="select-container">
+                    <select class="item__amount" name="" id="">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                    </select>
+                    <svg viewBox="0 0 20 10"><path d="M0,2 10,10 20,2 18,0 10,6 2,0"/></svg>
+                </div>
+                <div class="select-container">
+                    <select class="item__size" name="" id="">
+                        <option value="xs">XS</option>
+                        <option value="s">S</option>
+                        <option value="m">M</option>
+                        <option value="l">L</option>
+                        <option value="xl">XL</option>
+                    </select>
+                    <svg viewBox="0 0 20 10"><path d="M0,2 10,10 20,2 18,0 10,6 2,0"/></svg>
+                </div>
+                <button class="item__btn"><img src="./img/swoosh.png" alt="loes" class="item__btn--img"> Bestel nu</button>
             </div>
         </div>
     </div>
     <div class="item">
-        <img src="https://via.placeholder.com/200x400" alt="weg, gone, loesoe">
+        <img src="https://via.placeholder.com/200x400?text=wide%20boy" alt="weg, gone, loesoe">
         <div class="item__body">
             <div class="item__info">
                 <h3 class="item__title">Ocean T-shirt</h3>
                 <span class="item__price">€35,00</span>
-                <select class="item__amount" name="" id="">
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                </select>
-                <select class="item__size" name="" id="">
-                    <option value="xs">Extra small</option>
-                    <option value="s">Small</option>
-                    <option value="m">Medium</option>
-                    <option value="l">Large</option>
-                    <option value="xl">Extra large</option>
-                </select>
-            </div>
-            <div class="item__btn-container">
-                <button class="item__btn"><img src="./img/swoosh.png" alt="loes" class="item__btn--img">Bestel nu</button>
+                <div class="select-container">
+                    <select class="item__amount" name="" id="">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                    </select>
+                    <svg viewBox="0 0 20 10"><path d="M0,2 10,10 20,2 18,0 10,6 2,0"/></svg>
+                </div>
+                <div class="select-container">
+                    <select class="item__size" name="" id="">
+                        <option value="xs">XS</option>
+                        <option value="s">S</option>
+                        <option value="m">M</option>
+                        <option value="l">L</option>
+                        <option value="xl">XL</option>
+                    </select>
+                    <svg viewBox="0 0 20 10"><path d="M0,2 10,10 20,2 18,0 10,6 2,0"/></svg>
+                </div>
+                <button class="item__btn"><img src="./img/swoosh.png" alt="loes" class="item__btn--img"> Bestel nu</button>
             </div>
         </div>
     </div>
@@ -97,59 +109,62 @@ Shop | Ocean Studios
 <div class="items sokken">
     <h2 class="title"><img src="./img/swoosh.png" alt="loes" class="title__img">Sokken</h2>
     <div class="item">
-        <img src="https://via.placeholder.com/200x400" alt="weg, gone, loesoe">
+        <img src="https://via.placeholder.com/200x400?text=wide%20boy" alt="weg, gone, loesoe">
         <div class="item__body">
             <div class="item__info">
-                <h3 class="item__title">Ocean sokken</h3>
+                <h3 class="item__title">Ocean T-shirt</h3>
                 <span class="item__price">€35,00</span>
-                <select class="item__amount" name="" id="">
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                </select>
-            </div>
-            <div class="item__btn-container">
-                <button class="item__btn"><img src="./img/swoosh.png" alt="loes" class="item__btn--img">Bestel nu</button>
+                <div class="select-container">
+                    <select class="item__amount" name="" id="">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                    </select>
+                    <svg viewBox="0 0 20 10"><path d="M0,2 10,10 20,2 18,0 10,6 2,0"/></svg>
+                </div>
+                <button class="item__btn"><img src="./img/swoosh.png" alt="loes" class="item__btn--img"> Bestel nu</button>
             </div>
         </div>
     </div>
     <div class="item">
-        <img src="https://via.placeholder.com/200x400" alt="weg, gone, loesoe">
+        <img src="https://via.placeholder.com/200x400?text=wide%20boy" alt="weg, gone, loesoe">
         <div class="item__body">
             <div class="item__info">
-                <h3 class="item__title">Ocean sokken</h3>
+                <h3 class="item__title">Ocean T-shirt</h3>
                 <span class="item__price">€35,00</span>
-                <select class="item__amount" name="" id="">
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                </select>
-            </div>
-            <div class="item__btn-container">
-                <button class="item__btn"><img src="./img/swoosh.png" alt="loes" class="item__btn--img">Bestel nu</button>
+                <div class="select-container">
+                    <select class="item__amount" name="" id="">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                    </select>
+                    <svg viewBox="0 0 20 10"><path d="M0,2 10,10 20,2 18,0 10,6 2,0"/></svg>
+                </div>
+                <button class="item__btn"><img src="./img/swoosh.png" alt="loes" class="item__btn--img"> Bestel nu</button>
             </div>
         </div>
     </div>
     <div class="item">
-        <img src="https://via.placeholder.com/200x400" alt="weg, gone, loesoe">
+        <img src="https://via.placeholder.com/200x400?text=wide%20boy" alt="weg, gone, loesoe">
         <div class="item__body">
             <div class="item__info">
-                <h3 class="item__title">Ocean sokken</h3>
+                <h3 class="item__title">Ocean T-shirt</h3>
                 <span class="item__price">€35,00</span>
-                <select class="item__amount" name="" id="">
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                </select>
-            </div>
-            <div class="item__btn-container">
-                <button class="item__btn"><img src="./img/swoosh.png" alt="loes" class="item__btn--img">Bestel nu</button>
+                <div class="select-container">
+                    <select class="item__amount" name="" id="">
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                    </select>
+                    <svg viewBox="0 0 20 10"><path d="M0,2 10,10 20,2 18,0 10,6 2,0"/></svg>
+                </div>
+                <button class="item__btn"><img src="./img/swoosh.png" alt="loes" class="item__btn--img"> Bestel nu</button>
             </div>
         </div>
     </div>
