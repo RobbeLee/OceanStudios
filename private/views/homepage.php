9d333ab752e317effc84e1c08e14b353757eb2e0
pk1260 <18251@ma-web.nl>
2019-09-25 16:12:31 +0200

    new stuff

--- private/views/homepage.php
+++ private/views/homepage.php
@@ -22,24 +22,14 @@ Ocean Studios
                 <img src="img/free.jpg" alt="free realestate" class="aboutus__main--img-left">
             </div>
             <div class="aboutus__main--section-right">
-                <img src="" alt="" class="aboutus__main--img-right">
                 <p class="aboutus__main--p">
+                    <img src="img/swoosh-2.png" alt="swoosh" class="aboutus__main--img-right">
                     Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean
                     massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec
                     quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
                     Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut,
                     imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt.
                     Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula,
-                    porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis,
-                    feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean
-                    imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.
-
-                    rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer
-                    tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo
-                    ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra
-                    quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean
-                    imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.
-                </p>
             </div>
             <h2 class="aboutus__main--h2">Wie zijn wij</h2>
         </div>
@@ -48,8 +38,8 @@ Ocean Studios
     <!-- Intermission part with Portfolio and Shop -->
     <div class="intermission__main">
         <div class="intermission__main--content">
-            <a href="#"><button class="left">Portfolio</button></a>
-            <a href="#"><button class="right">Portfolio</button></a>
+            <a href="#" class="left"><button>Portfolio</button></a>
+            <a href="#" class="right"><button>Portfolio</button></a>
         </div>
     </div>
 
@@ -58,17 +48,14 @@ Ocean Studios
         <div class="ourmission__main--content">
             <div class="ourmission__main--section-left">
                 <img src="" alt="" class="ourmission__main--img-left">
+                <h2 class="aboutus__main--h2">Wie zijn wij</h2>
                 <p class="ourmission__main--p">
-<!--                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean-->
-<!--                    massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec-->
-<!--                    quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.-->
-<!--                    Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo,-->
-<!---->
-<!--                    rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer-->
-<!--                    tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo-->
-<!--                    ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra-->
-<!--                    quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean-->
-<!--                    imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.-->
+                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean
+                    massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec
+                    quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.
+                    Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo,
+                    rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer
+                    tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.
                 </p>
             </div>
             <div class="ourmission__main--section-right">
