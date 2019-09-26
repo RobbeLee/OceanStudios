b35a00c1708ad3307feec734587b2416c5398468
Bjørn Emmaneel <27728@ma-web.nl>
2019-09-26 17:52:09 +0200

    Update

--- private/views/portfolio.php
+++ private/views/portfolio.php
@@ -28,7 +28,7 @@ Portfolio | Ocean Studios
 <div class="main__blauw">
     <div class="main__blauw--img">
         <!--   Place a screenshot of your website in the portfolio.css    -->
-        <img src="/OceanStudios/public/img/imageviewer.jpg" alt=" ">
+        <img src="/OceanStudios/public/img/Imageviewermobile.jpg" alt="">
     </div>
     <div class="main__blauw--content">
         <!--    Your name    -->
@@ -48,19 +48,58 @@ Portfolio | Ocean Studios
 <div class="main__wit">
     <div class="main__wit--img">
         <!--   Place here a screenshot of your website    -->
-        <img src="/img/OceanMobile.jpg" alt="">
+        <img src="/OceanStudios/public/img/portfolioJoppe.jpg" alt="">
+
     </div>
     <div class="main__wit--content">
         <!--    Your name    -->
-        <h2>Example Examples</h2>
+        <h2>Joppe Meijer - Portfolio</h2>
 
         <!--  Link to your website  -->
-        <a href="example.nl">example</a>
+        <a href="http://18325.hosts.ma-cloud.nl/portfolio/index.html">http://18325.hosts.ma-cloud.nl/portfolio/index.html</a>
         <br>
         <br>
 
         <!--    Here you explain or give a small introduction of your project    -->
-        <p>example of a example of a text</p>
+        <p>De portfolio van onze vormgever Joppe Meijer.</p>
+    </div>
+</div>
+
+<div class="main__blauw">
+    <div class="main__blauw--img">
+        <!--   Place here a screenshot of your website    -->
+        <img src="/OceanStudios/public/img/portfolioFengly.jpg" alt="">
+    </div>
+    <div class="main__blauw--content">
+        <!--    Your name    -->
+        <h2>Fengly Brasser - Portfolio</h2>
+
+        <!--  Link to your website  -->
+        <a href="https://fanglybrasser.wixsite.com/portfolio">https://fanglybrasser.wixsite.com/portfolio</a>
+        <br>
+        <br>
+
+        <!--    Here you explain or give a small introduction of your project    -->
+        <p>De portfolio van onze vormgeefster Fengly Brasser</p>
+    </div>
+</div>
+
+<div class="main__wit">
+    <div class="main__wit--img">
+        <!--   Place here a screenshot of your website    -->
+        <img src="/OceanStudios/public/img/portfolioMike.jpg" alt="">
+    </div>
+    <div class="main__wit--content">
+        <!--    Your name    -->
+        <h2>Mike Schaatsbergen</h2>
+
+        <!--  Link to your website  -->
+        <a href="http://25933.hosts2.ma-cloud.nl/">http://25933.hosts2.ma-cloud.nl/</a>
+        <br>
+        <br>
+
+        <!--    Here you explain or give a small introduction of your project    -->
+        <p>De portfolio van onze fullstack developer Mike Schaatsbergen</p>
     </div>
 </div>
 
@@ -89,6 +128,25 @@ Portfolio | Ocean Studios
         <img src="../../public/img/OceanMobile.jpg" alt="">
     </div>
     <div class="main__wit--content">
+        <!--    Your name    -->
+        <h2>Example Examples</h2>
+
+        <!--  Link to your website  -->
+        <a href="example.nl">example</a>
+        <br>
+        <br>
+
+        <!--    Here you explain or give a small introduction of your project    -->
+        <p>example of a example of a text</p>
+    </div>
+</div>
+
+<div class="main__blauw">
+    <div class="main__blauw--img">
+        <!--   Place here a screenshot of your website    -->
+        <img src="../../public/img/OceanMobile.jpg" alt="">
+    </div>
+    <div class="main__blauw--content">
         <!--    Your name    -->
         <h2>Example Examples</h2>
 
